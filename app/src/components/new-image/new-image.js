6308b5d2f9927549525703cc705f421401029af3
Michelle Sri <michelle.sri@gmail.com>
2016-12-12 22:27:37 -0800

    done adding scss styling

---
+++ app/src/components/new-image/new-image.js
@@ -0,0 +1,31 @@
+import template from './new-image.html';
+import styles from './new-image.scss';
+
+export default {
+  template,
+  bindings: {
+    add: '<'
+  },
+  controller
+};
+
+function controller(){
+  this.styles = styles;
+
+  this.reset = () => {
+    this.title = '';
+    this.description = '';
+    this.link = '';
+  };
+
+  this.reset();
+
+  this.addImage = () => {
+    this.add({
+      title: this.title,
+      description: this.description,
+      link: this.link
+    });
+    this.reset();
+  };
+}
