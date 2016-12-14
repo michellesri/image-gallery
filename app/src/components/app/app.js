58ccc01df964b5696e01dfef3861dac7436f502c
Michelle Sri <michelle.sri@gmail.com>
2016-12-14 12:57:48 -0800

    removed excessive code and added foo class styling for active tab

--- app/src/components/app/app.js
+++ app/src/components/app/app.js
@@ -2,12 +2,17 @@ import template from './app.html';
 import './app.scss';
 
 export default {
-  template,
-  controller
+  template
+  // controller
 };
 
-controller.$inject = ['$state'];
+// need routes.js file
+  // to invoke / go to a route, can use this function below, or can use sref.
 
-function controller($state) {
-  this.gotoImages = () => $state.go('images');
-}
+
+
+// controller.$inject = ['$state'];
+//
+// function controller($state) {
+//   this.gotoImages = () => $state.go('images');
+// }
