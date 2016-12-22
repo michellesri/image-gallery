977a771f2c1aad5986619f305bcb96f6829654ad
Michelle Sri <michelle.sri@gmail.com>
2016-12-21 23:58:24 -0800

    added protractor but still have some questions

---
+++ app/protractor.conf.js
@@ -0,0 +1,26 @@
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  //list of file globs
+  specs: [
+    'test-e2e/**/*.js'
+  ],
+
+  // or, define suites:
+
+  //suites: {
+  //   welcome: 'test-e2e/welcome.test.js',
+  //   full: 'test-e2e/**/*.test.js'
+  // },
+
+  capabilities: {
+    browserName: 'chrome'
+  },
+
+  // leave this out and protractor will start a server for us
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  baseUrl: 'http://localhost8080',
+
+  framework: 'jasmine'
+};
---
+++ app/test-e2e/protractorE2E.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('image gallery', function(){
+  beforeEach(function(){
+    browser.get('/');
+  });
+
+
+  it('has the correct title', function(){
+    browser.get('/');
+    expect(browser.getTitle()).toEqual('Michelle\'s Image Gallery');
+  });
+
+  describe('navigation', function(){
+    
+    it('defaults to welcome route', function(){
+      var uiView = element(by.css('ui-view'));
+
+      function testState(url, componentName){
+        expect(browser.getLocationAbsUrl()).toBe(url);
+        var component = uiView.all(by.css('*')).first();
+        expect(component.getTagName()).toEqual(componentName);
+      }
+      testState('/', 'welcome');
+
+      var nav = element.all(by.css('header a'));
+      var albumGallery = nav.get(1);
+
+      albumGallery.click();
+
+      testState('/images', 'images');
+
+    });
+
+  });
+});
